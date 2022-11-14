bc9c6851a3ed0f71ba6c5db7c5952fca072546d6
Visualizeit <17688172112@163.com>
2022-11-14 19:40:59 +0800

    feat: add HomePageMenu link

--- src/features/Home/components/HomePageMenu.vue
+++ src/features/Home/components/HomePageMenu.vue
@@ -14,13 +14,13 @@ import {
 } from '../assets'
 
 const menuItems: HomePageMenuItemProps[] = [
-  { name: '目前', iconSrc: codeComputerIcon },
-  { name: '导航', iconSrc: navigationIcon },
-  { name: '服务器', iconSrc: serverIcon },
-  { name: '成员', iconSrc: peoplesIcon },
+  { name: '目前', iconSrc: codeComputerIcon, isDeveloping: true },
+  { name: '导航', iconSrc: navigationIcon, path: '/sites' },
+  { name: '服务器', iconSrc: serverIcon, isDeveloping: true },
+  { name: '成员', iconSrc: peoplesIcon, path: '/members' },
   { name: '文档', iconSrc: bookIcon },
-  { name: '文件仓库', iconSrc: cloudStorageIcon },
-  { name: '成就', iconSrc: trophyIcon },
+  { name: '文件仓库', iconSrc: cloudStorageIcon, isDeveloping: true },
+  { name: '成就', iconSrc: trophyIcon, isDeveloping: true },
   { name: '待扩展区域', iconSrc: loadingIcon }
 ]
 </script>
