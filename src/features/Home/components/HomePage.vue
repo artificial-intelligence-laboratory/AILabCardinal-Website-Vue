019a9e8aa3384fdb19f75aaee5bf4a11f3741c9c
Visualizeit <17688172112@163.com>
2022-11-23 17:06:17 +0800

    fix: Do not display sign in button when not logged in

--- src/features/Home/components/HomePage.vue
+++ src/features/Home/components/HomePage.vue
@@ -3,6 +3,9 @@ import HomePageTitle from './HomePageTitle.vue'
 import HomePageMenu from './HomePageMenu.vue'
 import CheckInButton from '@/features/CheckIn/components/CheckInButton.vue'
 import LoginModal from '@/features/Auth/components/LoginModal.vue'
+import useAuthStore from '@/features/Auth/stores/useAuthStore'
+
+const auth = useAuthStore()
 </script>
 
 <template>
@@ -12,7 +15,7 @@ import LoginModal from '@/features/Auth/components/LoginModal.vue'
   </NLayoutHeader>
   <NLayoutContent>
     <div class="mt-10 space-y-5">
-      <CheckInButton />
+      <CheckInButton v-if="auth.isLoggedIn" />
       <HomePageMenu
     /></div>
   </NLayoutContent>
