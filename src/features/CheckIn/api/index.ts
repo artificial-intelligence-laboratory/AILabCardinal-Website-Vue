f694e9f1451597d5653a3ca38a18f7a499601e8d
Visualizeit <17688172112@163.com>
2022-11-23 15:22:39 +0800

    fix: signIn state no update

--- src/features/CheckIn/api/index.ts
+++ src/features/CheckIn/api/index.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/vue-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/vue-query'
 import { createQueryKeys } from '@lukemorales/query-key-factory'
 import type { AxiosError } from 'axios'
 import axios from '@/shared/axios'
@@ -16,9 +16,14 @@ const checkInKeys = createQueryKeys('checkIn', {
 })
 
 export const useCheckIn = () => {
+  const queryClient = useQueryClient()
+
   const mutation = useMutation({
     mutationFn(params: CheckInDTO) {
       return axios.post(USER_API_PATH + '/signIn', params)
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: checkInKeys.check().queryKey })
     }
   })
 
