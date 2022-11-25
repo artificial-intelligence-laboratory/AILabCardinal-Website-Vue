12a6bd4775494c3015af33bfcbbdd01a4310b8c3
Visualizeit <17688172112@163.com>
2022-11-25 17:22:12 +0800

    feat: add logout button

--- src/features/Members/components/UserInfoCard.vue
+++ src/features/Members/components/UserInfoCard.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
 import useAuthStore from '@/features/Auth/stores/useAuthStore'
+import { useLogout } from '@/features/Auth/api'
 import { collegeManAvatar } from '../assets'
 import GithubLink from './GithubLink.vue'
 
 const auth = useAuthStore()
+
+const logoutMutation = useLogout()
 </script>
 
 <template>
   <NCard v-if="auth.loginInfo" class="userInfoCard w-72" hoverable>
     <div class="flex items-center gap-x-4">
-      <NAvatar
-        round
-        class="ring ring-primary"
-        :src="collegeManAvatar"
-        :size="48" />
+      <NTooltip trigger="hover">
+        <template #trigger>
+          <button class="bg-transparent" @click="logoutMutation.mutate()">
+            <NAvatar
+              round
+              class="ring ring-primary"
+              :src="collegeManAvatar"
+              :size="48" /></button
+        ></template>
+        退出登录
+      </NTooltip>
       <p class="text-base">{{ auth.loginInfo.user.nickname }}</p>
     </div>
     <GithubLink :href="auth.loginInfo.user.githubUrl" />
